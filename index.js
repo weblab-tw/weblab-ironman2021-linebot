1a8012027cab588028bef02d60c0fb51dec65853
徐愷 <kkshyu.tw@gmail.com>
2021-10-10 17:21:47 -0400

    [BUGFIX] use reply instead of push message

--- index.js
+++ index.js
@@ -1,211 +1,172 @@
-require("dotenv").config();
-const Redis = require("ioredis");
-const cheerio = require("cheerio");
-const line = require("@line/bot-sdk");
-const { default: axios } = require("axios");
-const bodyParser = require("body-parser");
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 5000;
+require('dotenv').config()
+const Redis = require('ioredis')
+const cheerio = require('cheerio')
+const line = require('@line/bot-sdk')
+const { default: axios } = require('axios')
+const bodyParser = require('body-parser')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 const lineClient = new line.Client({
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-});
+})
 const redisClient = new Redis(process.env.REDIS_URL, {
   tls: {
     rejectUnauthorized: false,
   },
-});
+})
 
 const app = express()
   .use(bodyParser.json())
-  .use(express.static(path.join(__dirname, "public")))
-  .set("views", path.join(__dirname, "views"))
-  .set("view engine", "ejs");
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
 
 app
-  .get("/", async (req, res) => {
-    const teamIds = await getTeamIds();
+  .get('/', async (req, res) => {
+    const teamIds = await getTeamIds()
     return res.send(`<ul>
-    ${teamIds.map(
-      (teamId) => `<li><a href="/teams/${teamId}">Team ${teamId}</li>`
-    )}
-    </ul>`);
+    ${teamIds.map(teamId => `<li><a href="/teams/${teamId}">Team ${teamId}</li>`)}
+    </ul>`)
   })
-  .get("/teams/:teamId", async (req, res) => {
-    const teamId = req.params.teamId;
-    const force = Boolean(Number(new URLSearchParams(req.query).get("force")));
-    const articles = await fetchTeamArticles(teamId, force);
+  .get('/teams/:teamId', async (req, res) => {
+    const teamId = req.params.teamId
+    const force = Boolean(Number(new URLSearchParams(req.query).get('force')))
+    const articles = await fetchTeamArticles(teamId, force)
     for (let index = 0; index < articles.length; index++) {
-      const article = articles[index];
-      await redisClient.hset(`teams:${teamId}:articles:${article.id}`, article);
+      const article = articles[index]
+      await redisClient.hset(`teams:${teamId}:articles:${article.id}`, article)
     }
-    return res.send({ articles });
+    return res.send({ articles })
   })
-  .post("/callback", async (req, res) => {
+  .post('/callback', async (req, res) => {
     try {
       for (let index = 0; index < req.body.events.length; index++) {
-        const event = req.body.events[index];
-        console.log({ event });
-        if (
-          event.type === "message" &&
-          event.message.type === "text" &&
-          event.message.text.startsWith("setup")
-        ) {
-          const teamId = Number(event.message.text.split(" ").pop());
+        const event = req.body.events[index]
+        console.log({ event })
+        if (event.type === 'message' && event.message.type === 'text' && event.message.text.startsWith('check')) {
+          const teamId = Number(event.message.text.split(' ').pop())
           if (!teamId) {
             await lineClient.replyMessage(event.replyToken, {
-              type: "text",
-              text: "抓不到 Team ID",
-            });
+              type: 'text',
+              text: '抓不到 Team ID',
+            })
           } else {
             const id =
-              event.source.type === "group"
+              event.source.type === 'group'
                 ? event.source.groupId
-                : event.source.type === "room"
+                : event.source.type === 'room'
                 ? event.source.roomId
-                : event.source.userId;
-            await redisClient.sadd(`teams:${teamId}:receivers`, id);
+                : event.source.userId
+            await redisClient.sadd(`teams:${teamId}:receivers`, id)
             await lineClient.replyMessage(event.replyToken, {
-              type: "text",
-              text: `已設定 Team ID: ${teamId}`,
-            });
-            await postTeamStatus(teamId);
+              type: 'text',
+              text: `檢查 Team ${teamId} 文章中...`,
+            })
+            const statusMessage = await getTeamStatusMessage(teamId)
+            await lineClient.replyMessage(event.replyToken, statusMessage)
           }
         }
       }
-      return res;
+      return res
     } catch (error) {
-      console.error(error);
-      return res.send({ error });
+      console.error(error)
+      return res.send({ error })
     }
   })
-  .post("/check", async (req, res) => {
-    const teamIds = await getTeamIds();
-    const errors = [];
-    for (const teamId of teamIds) {
-      try {
-        await postTeamStatus(teamId);
-      } catch (error) {
-        errors.push({ teamId, error });
-      }
-    }
-    return res.send({ errors });
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-const fetchCurrentDay = async (teamId) => {
-  const response = await axios.get(
-    `https://ithelp.ithome.com.tw/2021ironman/signup/team/${teamId}`
-  );
-  const $ = cheerio.load(response.data);
-  const day = $(".team-dashboard__day").text().trim();
-  return day;
-};
+const fetchCurrentDay = async teamId => {
+  const response = await axios.get(`https://ithelp.ithome.com.tw/2021ironman/signup/team/${teamId}`)
+  const $ = cheerio.load(response.data)
+  const day = Number($('.team-dashboard__day').text().trim()) + 1
+  return day
+}
 
-const fetchTeamMembers = async (teamId) => {
-  const response = await axios.get(
-    `https://ithelp.ithome.com.tw/2021ironman/signup/team/${teamId}`
-  );
-  const $ = cheerio.load(response.data);
-  const members = $(".team-leader-info__name")
+const fetchTeamMembers = async teamId => {
+  const response = await axios.get(`https://ithelp.ithome.com.tw/2021ironman/signup/team/${teamId}`)
+  const $ = cheerio.load(response.data)
+  const members = $('.team-leader-info__name')
     .map((idx, el) => $(el).text())
-    .toArray();
-  return members;
-};
+    .toArray()
+  return members
+}
 
 const fetchTeamArticles = async (teamId, force = false, page = 1) => {
-  console.log({ teamId, force, page });
-  const response = await axios.get(
-    `https://ithelp.ithome.com.tw/2021ironman/signup/team/${teamId}?page=${page}`
-  );
-  const $ = cheerio.load(response.data);
-  const articles = $(".team-article .ir-list")
+  console.log({ teamId, force, page })
+  const response = await axios.get(`https://ithelp.ithome.com.tw/2021ironman/signup/team/${teamId}?page=${page}`)
+  const $ = cheerio.load(response.data)
+  const articles = $('.team-article .ir-list')
     .map((_, el) => {
-      const articleLink = $(el)
-        .children(".ir-list__title")
-        .children("a")
-        .attr().href;
-      const articleId = articleLink.split("/").pop();
+      const articleLink = $(el).children('.ir-list__title').children('a').attr().href
+      const articleId = articleLink.split('/').pop()
       const article = {
         id: articleId,
-        author: $(el)
-          .children(".ir-list__info")
-          .children(".ir-list__user")
-          .children("span.ir-list__name")
-          .html(),
-        title: $(el).children(".ir-list__title").children("a").html(),
+        author: $(el).children('.ir-list__info').children('.ir-list__user').children('span.ir-list__name').html(),
+        title: $(el).children('.ir-list__title').children('a').html(),
         link: articleLink,
         day: Number(
           $(el)
-            .children(".ir-list__group")
-            .children(".ir-list__group-topic")
-            .children("span.ir-list__group-topic-num")
+            .children('.ir-list__group')
+            .children('.ir-list__group-topic')
+            .children('span.ir-list__group-topic-num')
             .html()
-            .trim()
+            .trim(),
         ),
-      };
-      return article;
+      }
+      return article
     })
-    .toArray();
+    .toArray()
   if (articles.length === 0) {
-    return articles;
+    return articles
   }
   if (!force) {
-    const cachedTeamArticleKeys = await redisClient.keys(
-      `teams:${teamId}:articles:*`
-    );
-    let noNextPage = false;
+    const cachedTeamArticleKeys = await redisClient.keys(`teams:${teamId}:articles:*`)
+    let noNextPage = false
     for (let index = 0; index < cachedTeamArticleKeys.length; index++) {
-      const cachedTeamArticleKey = cachedTeamArticleKeys[index];
-      const cachedArticleId = cachedTeamArticleKey.split(":").pop();
-      if (!articles.find((article) => article.id === cachedArticleId)) {
-        const cachedArticle = await redisClient.hgetall(cachedTeamArticleKey);
-        articles.push(cachedArticle);
+      const cachedTeamArticleKey = cachedTeamArticleKeys[index]
+      const cachedArticleId = cachedTeamArticleKey.split(':').pop()
+      if (!articles.find(article => article.id === cachedArticleId)) {
+        const cachedArticle = await redisClient.hgetall(cachedTeamArticleKey)
+        articles.push(cachedArticle)
       } else {
-        noNextPage = true;
+        noNextPage = true
       }
     }
     if (noNextPage) {
-      return articles;
+      return articles
     }
   }
-  const restArticles = await fetchTeamArticles(teamId, force, page + 1);
-  restArticles.forEach((article) => articles.push(article));
-  return articles;
-};
+  const restArticles = await fetchTeamArticles(teamId, force, page + 1)
+  restArticles.forEach(article => articles.push(article))
+  return articles
+}
 
 const getTeamIds = async () => {
-  const teamIds = [];
-  const teamKeys = await redisClient.keys("teams:*:receivers");
-  teamKeys.forEach((teamKey) => teamIds.push(Number(teamKey.split(":")[1])));
-  return teamIds;
-};
+  const teamIds = []
+  const teamKeys = await redisClient.keys('teams:*:receivers')
+  teamKeys.forEach(teamKey => teamIds.push(Number(teamKey.split(':')[1])))
+  return teamIds
+}
 
-const postTeamStatus = async (teamId) => {
-  const currentDay = await fetchCurrentDay(teamId);
-  const members = await fetchTeamMembers(teamId);
-  const articles = await fetchTeamArticles(teamId);
-  const memberStatusList = members.map((member) => {
+const getTeamStatusMessage = async teamId => {
+  const currentDay = await fetchCurrentDay(teamId)
+  const members = await fetchTeamMembers(teamId)
+  const articles = await fetchTeamArticles(teamId)
+  const memberStatusList = members.map(member => {
     return {
       member,
-      articles: articles.filter((article) => article.author === member).length,
-    };
-  });
+      articles: articles.filter(article => article.author === member).length,
+    }
+  })
   const message = {
-    type: "text",
+    type: 'text',
     text:
       `# ${currentDay}\n` +
       `-------------------\n` +
-      memberStatusList
-        .map(
-          (memberStatus) => `${memberStatus.member}: ${memberStatus.articles}`
-        )
-        .join("\n") +
+      memberStatusList.map(memberStatus => `${memberStatus.member}: ${memberStatus.articles}`).join('\n') +
       `\n\n團隊連結：\nhttps://ithelp.ithome.com.tw/2021ironman/signup/team/${teamId}`,
-  };
-  const receivers = await redisClient.smembers(`teams:${teamId}:receivers`);
-  for (const receiver of receivers) {
-    await lineClient.pushMessage(receiver, message);
   }
-};
+  return message
+}
